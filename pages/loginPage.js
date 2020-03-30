163f8a38079b862bd8bfe909c58dd259e4092cac
danieldawid <daniel.dawid@contractors.roche.com>
2020-03-30 19:05:06 +0200

    Fixik

--- pages/loginPage.js
+++ pages/loginPage.js
@@ -1,7 +1,14 @@
+const { Builder, By, Key, until } = require("selenium-webdriver");
 class LoginPage {
-    navigateToFacebookLoginPage(driver){
-        driver.get('https://www.facebook.com/');
+    async navigateToFacebookLoginPage(driver) {
+        await driver.get('https://www.facebook.com/');
     }
-}
+    async loginToFacebook(driver, email, password) {
+        await driver.findElement(By.css('#email')).sendKeys(email);
+        await driver.findElement(By.css('#pass')).sendKeys(password);
+        await driver.findElement(By.css('#u_0_b')).click();
 
+    }
+
+}
 module.exports = new LoginPage();
