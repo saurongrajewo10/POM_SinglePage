7c7e49c4324c4fc3e5a126965a474a5506efc0fd
danieldawid <daniel.dawid@contractors.roche.com>
2020-03-30 19:02:00 +0200

    WorkingPassTyping

--- tests/others/TC01 Loging.js
+++ tests/others/TC01 Loging.js
@@ -1,6 +1,10 @@
 var loginPage = require('../../pages/loginPage');
 var webdriver = require('selenium-webdriver');
 
-driver = new webdriver.Builder().withCapabilities(webdriver.Capabilities.chrome()).build();
+async function Loging() {
+    driver = new webdriver.Builder().withCapabilities(webdriver.Capabilities.chrome()).build();
 
-loginPage.navigateToFacebookLoginPage(driver);
+    await loginPage.navigateToFacebookLoginPage(driver);
+    await loginPage.loginToFacebook(driver, 'dajto', 'wciapkach');
+}
+Loging();
