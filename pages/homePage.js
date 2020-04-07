c2e2bc76b8e35009e3d7f6544779b2cfeabe46d3
Zbigniew Kuminski <zbigniew.kuminski@gmail.com>
2020-04-07 17:40:54 +0200

    TC01 fix

--- pages/homePage.js
+++ pages/homePage.js
@@ -1,3 +1,4 @@
+const { By } = require("selenium-webdriver");
 class HomePage {
     async returnHomePageUrl(driver) {
         while (driver.getCurrentUrl() === 'https://www.facebook.com/login/device-based/regular/login/?login_attempt=1&lwv=110') {
@@ -5,5 +6,8 @@ class HomePage {
         return await driver.getCurrentUrl();
     };
 
+    async closeGrayScreen(driver){
+        await driver.findElement(By.css('body > div._n8._3qx.uiLayer._3qw')).click();
+    }
 }
 module.exports = new HomePage();
--- tests/TC01 Loging.js
+++ tests/TC01 Loging.js
@@ -1,8 +1,8 @@
 const assert = require("assert");
-var loginPage = require('../../pages/loginPage');
+var loginPage = require('../pages/loginPage');
 var webdriver = require('selenium-webdriver');
-var failedLoginPage = require('../../pages/failedLoginPage');
-var homePage = require('../../pages/homePage');
+var failedLoginPage = require('../pages/failedLoginPage');
+var homePage = require('../pages/homePage');
 
 async function Loging() {
     try {
@@ -13,7 +13,8 @@ async function Loging() {
         await assert.equal(await failedLoginPage.returnFailedLoginPageUrl(driver), 'https://www.facebook.com/login/device-based/regular/login/?login_attempt=1&lwv=110', 'Zjebao sie');
         await assert.ok(await failedLoginPage.returnLoginErrorStatement(driver));
         await failedLoginPage.loginToFacebook(driver, 'pnxvtavkiszxtapzfe@ttirv.org', 'Mars4321');
-        await assert.equal(await homePage.returnHomePageUrl(driver), 'https://www.facebook.com/')
+        await assert.equal(await homePage.returnHomePageUrl(driver), 'https://www.facebook.com/');
+        await homePage.closeGrayScreen(driver);
     }
     catch (error) {
         console.log(error);
