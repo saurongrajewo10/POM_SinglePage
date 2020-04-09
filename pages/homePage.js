2097749b67d726a558d55e787660c83087edd0cf
Zbigniew Kuminski <zbigniew.kuminski@gmail.com>
2020-04-09 17:22:33 +0200

    WHYle

--- pages/homePage.js
+++ pages/homePage.js
@@ -1,4 +1,7 @@
 const { By, until } = require("selenium-webdriver");
+
+const sleep = (milliseconds) => new Promise(resolve => setTimeout(resolve, milliseconds));
+
 class HomePage {
     async returnHomePageUrl(driver) {
         while (driver.getCurrentUrl() === 'https://www.facebook.com/login/device-based/regular/login/?login_attempt=1&lwv=110') {
@@ -13,8 +16,18 @@ class HomePage {
     async postPublish(driver){
         var postInput = await driver.findElement(By.className('_3en1 _480e navigationFocus'));
         await postInput.click();
-        await postInput.sendKeys('Test post');
+        await postInput.sendKeys('Test post' + Math.random(1,100));
         await driver.findElement(By.css('div._1j2v > div._2dck._1pek._4-u3 > div._45wg._69yt > button')).click();
+    }
+
+    async checkWhenPostAdded(driver){ 
+        var whenAdded = driver.findElement(By.className('timestampContent')).getText();
+        return whenAdded;
+    }
+
+    async waitUntilPostIsFound(driver){
+        while (await this.checkWhenPostAdded(driver) !== 'Przed chwilą') {
+        }
     }
 }
 module.exports = new HomePage();
