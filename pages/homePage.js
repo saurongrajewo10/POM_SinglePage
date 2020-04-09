6ac735f65dfa837c3b63e428fa93bd68352b029b
danieldawid <daniel.dawid@contractors.roche.com>
2020-04-09 18:34:29 +0200

    Almost finished TC04

--- pages/homePage.js
+++ pages/homePage.js
@@ -6,15 +6,27 @@ class HomePage {
         return await driver.getCurrentUrl();
     };
 
-    async closeGrayScreen(driver){
+    async closeGrayScreen(driver) {
         await driver.findElement(By.css('body > div._n8._3qx.uiLayer._3qw')).click();
     }
 
-    async postPublish(driver){
+    async postPublish(driver) {
         var postInput = await driver.findElement(By.className('_3en1 _480e navigationFocus'));
         await postInput.click();
         await postInput.sendKeys('Test post');
         await driver.findElement(By.css('div._1j2v > div._2dck._1pek._4-u3 > div._45wg._69yt > button')).click();
     }
+    async changeLanguage(driver) {
+        await driver.findElement(By.css('#pagelet_rhc_footer > div > div.uiContextualLayerParent > div > div > div._4bl9 > div > a:nth-child(3)')).click();
+        await driver.findElement(By.css('body > div._10.uiLayer._4-hy._3qw > div._59s7 > div > div > div > div._5lnf.uiOverlayFooter._5a8u > button')).click();
+        await this.closeGrayScreen(driver);
+        let languageToCompareAfterLanguageChange = this.checkLanguageText(driver, '#pagelet_rhc_footer > div > div.uiContextualLayerParent > div > div > div._4bl9 > div > a:nth-child(3)');
+        return languageToCompareAfterLanguageChange;
+    }
+    async checkLanguageText(driver, cssSelector) {
+        let languageWeWantToChangeTo = await driver.findElement(By.css(cssSelector)).getText();
+        return languageWeWantToChangeTo;
+    }
+
 }
 module.exports = new HomePage();
---
+++ tests/TC04 SearchingForUser.js
@@ -0,0 +1,18 @@
+var webdriver = require('selenium-webdriver');
+var successfullLogingFunction = require('./reusable functions/successfullLogingFunction');
+var homePage = require('../pages/homePage');
+const assert = require("assert");
+
+async function searchingForUser() {
+    try {
+        driver = new webdriver.Builder().withCapabilities(webdriver.Capabilities.chrome()).build();
+        await driver.manage().setTimeouts({ implicit: 5000 });
+        await successfullLogingFunction.successfullLogingFunction(driver);
+        await assert.equal(await homePage.returnHomePageUrl(driver), 'https://www.facebook.com/');
+        await homePage.searchForUser(driver, 'Zbigniew Kumiński');
+
+    } catch (error) {
+        console.log(error);
+    }
+}
+searchingForUser();
