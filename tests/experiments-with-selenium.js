15f664179febf3b7c9e5cd0a4a7ece7b1662e61e
danieldawid <daniel.dawid@contractors.roche.com>
2020-06-02 18:00:58 +0200

    Minor changes and fixes

--- tests/experiments-with-selenium.js
+++ tests/experiments-with-selenium.js
@@ -4,6 +4,7 @@ var loginPage = require('../pages/loginPage');
 var homePage = require('../pages/homePage');
 var user = require('./../tests/users/users.json');
 var actions = require('selenium-webdriver/lib/input');
+var WebElement = require('selenium-webdriver');
 const { Builder, By, Key, until, Actions, mouseDown } = require("selenium-webdriver");
 
 async function experiments() {
@@ -30,9 +31,7 @@ async function experiments() {
     // await alert.accept();
     // OPEN AN ALERT AND ACCEPT
 
-
     // const actions = await new actions.Actions();
-    // console.log('stryng');
     // await actions
     // .keyDown(ALT)
     // .keyDown(F4)
@@ -43,10 +42,9 @@ async function experiments() {
     // It is not working
 
     // TAKE A SCREENSHOT AND RETURN BASE ENCODED STRING (64 BASE CODED)
-    // let elo = await driver.takeScreenshot();
-    // console.log(elo);
+    // let screen = await driver.takeScreenshot();
+    // console.log(screen);
     // TAKE A SCREENSHOT AND RETURN BASE ENCODED STRING (64 BASE CODED)
-
 
     // RETURN A PAGE SOURCE
     // let HTMLCode = await driver.getPageSource();
@@ -54,10 +52,10 @@ async function experiments() {
     // RETURN A PAGE SOURCE
 
     // RETURN A OBJECT WITH SIZE AND POSITION OF WEBELEMENT
-    // let lokejszyn = await driver.findElement(By.className('_4un6')).getRect();
-    // console.log(lokejszyn)
+    // let elementLocation = await driver.findElement(By.className('_4un6')).getRect();
+    // console.log(elementLocation)
     // RETURN A OBJECT WITH SIZE AND POSITION OF WEBELEMENT
-    
+
     // CHECK IF CHECKBOX IS SELECTED
     // let radioButton = await driver.findElement(By.css('#main > div.w3-row > div:nth-child(1) > input[type=checkbox]:nth-child(2)'));
     // await radioButton.click();
@@ -68,6 +66,15 @@ async function experiments() {
     // let webElements = await driver.findElements(By.className('_4kny _2s24'));
     // console.log(webElements)
     // RETURNS AN ARRAY OF MATCHING WEBELEMENTS
+
+    let elemFirst = await driver.findElement(By.css('#stories_tray > div > div > div._7kft'));
+    let elemSecond = await driver.findElement(By.css('#stories_tray > div > div > div._7kft'));
+
+    //let compareIsEqual = driver.findElement(By.css('#stories_tray > div > div > div._7kft')).equals(elemFirst, elemSecond);
+    let comparision = new WebElement(driver, 'stryng');
+    comparision.equals(elemFirst, elemSecond)
+    console.log(comparision)
+   // console.log(elemFirst.equals(elemSecond));
 
 }
 experiments();
